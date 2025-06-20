b87e5e8033f479230dc04735d6cc88dee3ffb18b
CosAvr <avramidisko@gmail.com>
2025-06-20 12:22:20 +0300

    Local changes before syncing with remote

--- android/app/src/main/kotlin/com/example/first_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/first_app/MainActivity.kt
@@ -1,5 +1,13 @@
+<<<<<<< HEAD
 package com.example.first_app
 
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity : FlutterActivity()
+=======
+package com.example.first_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
+>>>>>>> 578562f ( new files uplaod)
